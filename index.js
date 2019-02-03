fb3cbddd57900a0ba2dc3cb21e8ca3bc69848511
unknown <shoaibmuhammedshaikh@gmail.com>
2019-02-03 14:32:18 -0500

    Switch to Node.js and add telegram bot functionality

---
+++ index.js
@@ -0,0 +1,51 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+const TelegramBotAPI = require('node-telegram-bot-api');
+const { TOKEN, URL } = require('./private/credentials');
+
+const raptorsTelegramBot = new TelegramBotAPI(TOKEN, { polling: true });
+
+async function getScoreData() {
+    const html = await rp(URL);
+    const unparsedData = $('.game-status__past', html).text().split(' ');
+    console.log(unparsedData);
+    let parsedData = [];
+    for (let i = 0; i < unparsedData.length; i++) {
+        if (unparsedData[i] === 'L' || unparsedData[i] === 'W') {
+            continue;
+        }
+        if (unparsedData[i].includes('L') || unparsedData[i].includes('W')) {
+            parsedData.push(unparsedData[i].slice(0, unparsedData[i].length - 1))
+        }
+    }
+    parsedData.push(unparsedData[unparsedData.length - 1]);
+    return parsedData;
+}
+
+async function getTeamData() {
+    const html = await rp(URL);
+    const unparsedData = $('img.logo', html);
+    let parsedData = []
+    unparsedData.each((index, element) => {
+        parsedData.push($(element).attr('alt').toUpperCase());
+    });
+    return parsedData
+}
+
+async function getLastTenGames() {
+    const scores = await getScoreData();
+    const teams = await getTeamData();
+    let output = 'Last 10 Games: \n'
+    for (i = scores.length - 10; i < scores.length ; i++) {
+        output += `RAPTORS ${scores[i]} ${teams[i]}\n`
+    }
+    return output
+
+}
+
+raptorsTelegramBot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    if (msg.text.toString() === '/lastTenGames') {
+        raptorsTelegramBot.sendMessage(chatId, await getLastTenGames())
+    }
+});
