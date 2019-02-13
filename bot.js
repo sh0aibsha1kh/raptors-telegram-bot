a4c2cadd06eb6c7da6115052989a3ee394247d36
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-02-13 11:45:33 -0500

    Remove host and add polling

--- bot.js
+++ bot.js
@@ -2,9 +2,8 @@ const TelegramBotAPI = require('node-telegram-bot-api');
 const { TOKEN } = require('./private/credentials');
 const { getNextGame, getNLastGames, getNumberOfGamesPlayed } = require('./methods/parser');
 
-const port = process.env.PORT || 8443;
-const host = process.env.HOST;
-const raptorsTelegramBot = new TelegramBotAPI(TOKEN, {webHook: {port: port, host: host}});
+const port = process.env.PORT;
+const raptorsTelegramBot = new TelegramBotAPI(TOKEN, {webHook: {port: port}, polling: true});
 
 raptorsTelegramBot.onText(/\/last(\d*)/, async (msg, match) => {
     const chatId = msg.chat.id;
