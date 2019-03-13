5411c7f5d17442b9c98990701e22409d6605da87
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-03-12 22:28:35 -0400

    Debug locations

--- methods/parser.js
+++ methods/parser.js
@@ -57,6 +57,7 @@ const getNextNGames = async n => {
     const locations = await scrapeLocations();
     let output = `_Next${n > 1 ? ' ' + n + ' ' : ' '}Game${n > 1 ? 's' : ''}_: \n\n`;
     for (i = 0; i < n; i++) {
+        console.log(locations[scoreLength + 1]);
         if (locations[scoreLength + i] === 'SCOTIABANK ARENA, TORONTO, ON') {
             output += `RAPTORS vs ${teams[scoreLength + i]} on ${dates[scoreLength + i]} @ ${times[i]}\n`;
         } else {
